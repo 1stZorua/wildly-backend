13686ab2a1ca1ec310f8c7b9cfd316416ddd5799
Zorua <milandehaart@live.nl>
2025-01-09 16:21:51 +0100

    Added comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#
 from flask import Flask, request, jsonify
 from keras._tf_keras.keras.models import load_model
 from utils import load_class_names, prepare_image
@@ -7,6 +8,8 @@ MODEL_PATH = 'models/dog_classifier_breed.keras'
 CLASSES_PATH = 'models/classes.json'
 
 app = Flask(__name__)
+
+# Load the pre-trained model and class names
 model = load_model(MODEL_PATH)
 classes = load_class_names(CLASSES_PATH)
 
@@ -16,22 +19,27 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    # Check if the request contains an image
     if 'image' not in request.files:
         return jsonify({'error': 'No file part'})
     
     image = request.files['image']
 
+    # Check if the image file is empty
     if image.filename == '':
         return jsonify({'error': 'No selected file'})
 
+    # Prepare the image for prediction
     img = prepare_image(image)
+    # Get prediction from the model
     prediction = model.predict(img)
 
+    # Find the index of the highest prediction
     predicted_index = np.argmax(prediction)
     confidence = float(prediction[0][predicted_index] * 100) 
 
+    # Get the top 3 predirected breeds
     top_3_indices = np.argsort(prediction)[0][-3:][::-1]
-
     top_3_breeds = [
         {
             'breed': classes[i],
@@ -40,6 +48,7 @@ def predict():
         for i in top_3_indices
     ]
 
+    # Return the prediction results
     return jsonify({
         'predicted_breed': classes[predicted_index],
         'confidence': round(confidence, 2),
